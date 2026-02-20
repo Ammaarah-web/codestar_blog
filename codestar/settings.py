0a96b32edd7de357e5020dd3abe1179446780fff
Ammaarah <b.ammaarah101@gmail.com>
2026-02-20 09:03:50 +0000

    added a missing comma

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l%8kwm43mv)k&-4ouv0(9an42b-%zdayefn%i$=6zw2j9j4-i0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = [,'.herokuapp.com']
 
 
 # Application definition
